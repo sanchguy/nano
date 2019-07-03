ec8a5da352a689f2745f0af6c35217816c9ffa96
sanch <sanchguy@gmail.com>
2019-07-03 18:15:34 +0800

    0703commit

--- model/manager.go
+++ model/manager.go
@@ -70,6 +70,50 @@ func (m *Manager)Login(s *session.Session,req *protocol.LoginToGameServerRequest
 	log.Infof("玩家:%d登录： %v",uid,req)
 	if p,ok := m.player(uid); !ok{
 		log.Infof("玩家：%d不在线，创建新玩家",uid)
-		p =
+		p = NewPlayer(s,uid,req.Name)
+		m.setPlayer(uid,p)
+	}else {
+		log.Infof("玩家:%d已经在线",uid)
+		//移除广播频道
+		m.group.Leave(s)
+
+		//重置之前的session
+		if prevSession := p.session;prevSession != nil && prevSession != s {
+			//如果之前房间存在，则退出来
+			if p , err := playerWithSession(prevSession); err == nil && p != nil && p.room != nil && p.room.group != nil {
+				p.room.group.Leave(prevSession)
+			}
+			prevSession.Clear()
+			prevSession.Close()
+		}
+
+		//绑定新session
+		p.bindSession(s)
 	}
+	m.group.Add(s)
+
+	res := &protocol.LoginToGameServerResponse{
+		Uid:s.UID(),
+		Nickname:req.Name,
+		Sex:req.Sex,
+		HeadUrl:req.HeadUrl,
+	}
+
+	return s.Response(res)
+}
+
+func (m *Manager) setPlayer(uid int64, p *Player) {
+	if _, ok := m.players[uid]; ok {
+		log.Warnf("玩家已经存在，正在覆盖玩家， UID=%d", uid)
+	}
+	m.players[uid] = p
+}
+
+func (m *Manager) sessionCount() int {
+	return len(m.players)
+}
+
+func (m *Manager) offline(uid int64) {
+	delete(m.players, uid)
+	log.Infof("玩家: %d从在线列表中删除, 剩余：%d", uid, len(m.players))
 }
